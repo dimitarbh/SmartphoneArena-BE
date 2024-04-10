59704015db226f489b57afd5c5928b9c5a32b1da
Stefanov <konstantin.stefanov@dxc.com>
2024-04-10 14:26:50 +0300

    fixed

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
-
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
@@ -9,4 +8,4 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+export default User;
