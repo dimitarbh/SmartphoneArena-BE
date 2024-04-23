8dbf444213eccfc35ab3f409e6872fc85aaefe8c
Dimitar Hadzhiyanchev <dimitarbh@gmail.com>
2024-04-23 18:19:56 +0300

    test

--- routes/models.js
+++ routes/models.js
@@ -21,6 +21,7 @@ router.get('/:id', async (req, res) => {
         console.error(error)
         res.status(500).json({message: 'Server error'})
     }
+    
 })
 
 router.post('/', async (req, res) => {
