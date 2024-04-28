0fea7880281d52e8466442345559b55a9cfa2ccc
Dimitar Hadzhiyanchev <dimitarbh@gmail.com>
2024-04-28 18:00:52 +0300

    update

--- routes/brands.js
+++ routes/brands.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import brands from '../models/Brand.js'
+import brandModels from '../models/brandCurrentModel.js'
 
 const router = express.Router()
 
@@ -29,13 +30,13 @@ router.get('/:brandId', async (req, res) => {
 })
 
 
-router.get('/allBrandModels', async (req, res) => {
+router.get('/allBrandModels/:brandId', async (req, res) => {
     try {
         const brandId = req.query.brandId;
         if(!brandId) {
             return res.status(400).send('id is required')
         }
-        const models = await brandModels.find(req.params.brandId);
+        const models = await brandModels.find({ brand: brandId }).populate('brand')
         res.status(201).json({message: 'Model received', models})
     } catch(error) {
         console.error(error)
