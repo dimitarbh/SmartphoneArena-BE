7d65503b8e4b044edc74e5039af4318d9de4da8b
Konstantin Stefanov <konstantin.stefanov87@gmail.com>
2024-05-01 09:35:07 +0300

    Update Brand.js

--- models/Brand.js
+++ models/Brand.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const BrandSchema = new mongoose.Schema({
     image: { type: String, required: true },
-    brand: {type: String, required: true, unique: true}
+    brand: { type: mongoose.Schema.Types.ObjectId, ref: 'Brand' }
 })
 
 const Brand = mongoose.model('Brand', BrandSchema, 'Brand')
